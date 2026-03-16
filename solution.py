c65bc54687b7fa5c0aba97de825f0093a7f3beb2
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-15 21:43:54 -0700

    Fix bugs with clues from Claude Code

--- solution.py
+++ solution.py
@@ -1,10 +1,14 @@
 def zero_duplicate(inventory_list):
         check = 0
-        while inventory_list:
+        while check < (len(inventory_list) - 1):
                 if inventory_list[check] == 0:
                         inventory_list.pop()
                         check += 1
-                        inventory_list[check].insert(0)
+                        inventory_list.insert(check, 0)
                         check += 1
                 else:
                         check += 1
+        return inventory_list
+
+if __name__ == '__main__':
+        print(zero_duplicate([4,0,1,3,0,2,5,0]))
