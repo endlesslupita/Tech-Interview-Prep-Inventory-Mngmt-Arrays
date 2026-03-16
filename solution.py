1a8f274210c99b1509531f8885be3e25d67122a5
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-16 08:00:41 -0700

    Style fix and add return with modified logic

--- solution.py
+++ solution.py
@@ -20,40 +20,37 @@ if __name__ == '__main__':
     print(zero_duplicate([4,0,1,3,0,2,5,0]))
 
 def zero_duplicate_optimized(inventory):
-    sum = 0
+    count = 0
     left = 0
     flag = 0
     list_length = len(inventory)
-    while sum < list_length:
+    right = list_length - 1
+
+    while count < list_length:
         if inventory[left] == 0:
-            if sum + 2 > list_length:
+            if count + 2 > list_length:
                 flag = 1
-                sum += 1
+                count += 1
             else:
-                sum += 2
+                count += 2
         else:
-            sum += 1
+            count += 1
         left += 1
     
     if left != 0:
         left -= 1
     
-    #when sum == list_length
-    if list_length != 0:
-        right = list_length - 1
-    else:
-        right = 0 #break here?
-    
 
-    while left >= 0:
-        if inventory[left] == 0 and flag == 0:
-            inventory[right] = 0
+    if right >= 0:
+        while left >= 0:
+            if inventory[left] == 0 and flag == 0:
+                inventory[right] = 0
+                right -= 1
+                inventory[right] = 0
+            else:
+                inventory[right] = inventory[left]
+                flag = 0
             right -= 1
-            inventory[right] = 0
-        else:
-            inventory[right] = inventory[left]
-            flag = 0
-        right -= 1
-        left -= 1
+            left -= 1
 
-    
+    return inventory
